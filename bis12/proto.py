a5212f2c7449bdef1d1800490c00d41ca1374b4f
Brian Stack <bis12@case.edu>
2012-04-21 19:24:11 -0400

    whatever

--- bis12/proto.py
+++ bis12/proto.py
@@ -4,8 +4,9 @@
 import sys
 import json
 
-n = 3
+n = 2
 index = {}
+stor = {}
 
 def clean(text):
   return (
@@ -29,16 +30,17 @@ def gramify_me_captain(line):
   spl = line.split()
   grams = []
   for word in spl:
-    for i in xrange(1,n+2):
+    for i in xrange(2,n+1):
       for j in xrange(len(word)-i+1):
         grams.append( word[j:j+i] )
   return grams
 
 def add_to_index(_id, grams):
+  stor[len(stor)] = _id
   for gram in grams:
     if gram not in index:
       index[gram] = set()
-    index[gram].add(_id)
+    index[gram].add(len(stor)-1)
 
 def query(inpt,q):
   results = set()
@@ -52,7 +54,10 @@ def query(inpt,q):
       if (w in index and r not in index[w]) or w not in index:
         rem.add(r)
   results = results.difference(rem)
-  sys.stdout.write( json.dumps(list(results)) )
+  res = []
+  for i in results:
+    res.append(stor[i])
+  sys.stdout.write( json.dumps(res) )
 
 if __name__ == '__main__':
   fs = sys.argv[1]
