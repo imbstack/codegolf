18ebb9980fb4a28baca301fcd7ac024dc1b8ff38
Brian Stack <bis12@case.edu>
2012-04-21 23:35:20 -0400

    ff

--- bis12/proto.py
+++ bis12/proto.py
@@ -4,9 +4,10 @@
 import sys
 import json
 
-n = 2
+n = 3
 index = {}
 stor = {}
+check = []
 
 def clean(text):
   return (
@@ -30,8 +31,8 @@ def gramify_me_captain(line):
   spl = line.split()
   grams = []
   for word in spl:
-    for i in xrange(2,n+1):
-      for j in xrange(len(word)-i+1):
+    for i in xrange(1,n+1):
+      for j in xrange(0, len(word)-i+1):
         grams.append( word[j:j+i] )
   return grams
 
@@ -39,19 +40,23 @@ def add_to_index(_id, grams):
   stor[len(stor)] = _id
   for gram in grams:
     if gram not in index:
-      index[gram] = set()
-    index[gram].add(len(stor)-1)
+      index[gram] = []
+    if (len(stor)-1) not in index:
+      index[gram].append(len(stor)-1)
 
 def query(inpt,q):
-  results = set()
-  results.update(index[inpt[0]])
+  if inpt[0] in index:
+    results = set(index[inpt[0]])
+  else:
+    sys.stdout.write(json.dump([]))
+    return
   for wd in inpt:
     if wd in index:
-      results = results.intersection(index[wd])
+      results = results.intersection(set(index[wd]))
   rem = set()
   for r in results:
     for w in q.split():
-      if (w in index and r not in index[w]) or w not in index:
+      if w not in check[r]:
         rem.add(r)
   results = results.difference(rem)
   res = []
@@ -68,6 +73,7 @@ if __name__ == '__main__':
     spl = line.split(':')
     _id = spl[0].strip()
     text = ' '.join(spl[1:])
+    check.append(clean(text))
     grams = gramify_me_captain(text)
     add_to_index(_id,grams)
 
