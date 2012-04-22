0f26e09c5814f054dea125dd4ce4c8b0eadc8d70
Brian Stack <bis12@case.edu>
2012-04-22 18:39:04 -0400

    Close

--- bis12/proto.py
+++ bis12/proto.py
@@ -3,11 +3,19 @@
 
 import sys
 import json
+import heapq
+from operator import itemgetter
+import cProfile
 
-n = 3
+min_n = 3
+max_n = 3
+tune_freq = 2000
+tune_part = 0.98
 index = {}
 stor = {}
-check = []
+check = {}
+freq = {}
+lkup = {}
 
 def clean(text):
   return (
@@ -26,29 +34,43 @@ def clean(text):
     .replace('!', ' !')
     .replace('-', ' - '))
 
-def gramify_me_captain(line):
+def gramify_me_captain(line,wc):
   line = clean(line)
   spl = line.split()
-  grams = []
+  grams = set()
   for word in spl:
-    for i in xrange(1,n+1):
-      for j in xrange(0, len(word)-i+1):
-        grams.append( word[j:j+i] )
+    for i in xrange(min_n-1,max_n+1):
+      for j in xrange(0, len(word)-i):
+        wd =  word[j:j+i+1]
+        if wd not in lkup:
+          lkup[wd] = wc
+          wc += 1
+        grams.add( lkup[wd] )
   return grams
 
-def add_to_index(_id, grams):
-  stor[len(stor)] = _id
+def add_to_index(_id, grams, tid):
+  stor[tid] = _id
   for gram in grams:
     if gram not in index:
-      index[gram] = []
-    if (len(stor)-1) not in index:
-      index[gram].append(len(stor)-1)
+      freq[gram] = 0
+      index[gram] = set()
+    else:
+      freq[gram] += 1
+    if tid not in index[gram]:
+      #if str(type(index[gram])) == "<type 'list'>":
+      #  index[gram] = set(index[gram])
+      index[gram].add(tid)
 
 def query(inpt,q):
-  if inpt[0] in index:
-    results = set(index[inpt[0]])
+  try:
+    first = inpt.pop()
+  except:
+    return
+  if first in index:
+    results = set(index[first])
   else:
-    sys.stdout.write(json.dump([]))
+    r = []
+    sys.stdout.write( json.dumps(r) )
     return
   for wd in inpt:
     if wd in index:
@@ -64,18 +86,31 @@ def query(inpt,q):
     res.append(stor[i])
   sys.stdout.write( json.dumps(res) )
 
+def compact():
+  tab = heapq.nsmallest(int(tune_part*len(freq)),freq.iteritems(), key=itemgetter(1))
+  for pair in tab:
+    if str(type(index[pair[0]])) != "<type 'list'>":
+      index[pair[0]] = list(index[pair[0]])
+
 if __name__ == '__main__':
   fs = sys.argv[1]
 
   f = open(fs,'r')
 
+  i = 0
+  j = 0
   for line in f:
+    i+=1
     spl = line.split(':')
     _id = spl[0].strip()
     text = ' '.join(spl[1:])
-    check.append(clean(text))
-    grams = gramify_me_captain(text)
-    add_to_index(_id,grams)
+    check[i] = clean(text)
+    grams = gramify_me_captain(text,j)
+    add_to_index(_id,grams,i)
+    if i%1000 == 0:
+      sys.stderr.write('\nAdded %d documents to index'%(i,))
+    #if i%tune_freq == 0:
+    #  compact()
 
   while True:
     sys.stdout.write('> '); sys.stdout.flush()
@@ -83,6 +118,8 @@ if __name__ == '__main__':
     except: break;
     if not inpt: continue
     inpt = clean(inpt)
-    grm = gramify_me_captain(inpt)
+    grm = gramify_me_captain(inpt,j)
     query(grm,inpt)
   sys.stdout.flush()
+
+
